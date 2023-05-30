10f62b5f8f4e6048eb88e59a8dc60bdbbaa836b3
Muhammad Agung Nugroho <nugroho.agung.m@gmail.com>
2023-05-30 11:35:10 +0700

    update

---
+++ app/index.php
@@ -0,0 +1,28 @@
+<?php
+
+$host = "mysql"; //
+$dbname = "my-website";
+$charset = "utf8";
+$port = "3306";
+
+try {
+    $pdo = new PDO(
+        dsn: "mysql:host=$host;dbname=$dbname;charset=$charset;port=$port",
+        username: "root",
+        password: "password123",
+    );
+
+    $persons = $pdo->query("SELECT * FROM Persons");
+
+    echo '<pre>';
+    foreach ($persons->fetchAll(PDO::FETCH_ASSOC) as $person) {
+        print_r($person);
+    }
+    echo '</pre>';
+
+} catch (PDOException $e) {
+    throw new PDOException(
+        message: $e->getMessage(),
+        code: (int)$e->getCode()
+    );
+}
